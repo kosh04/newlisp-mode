e2192a5ee58c7896fe2cd2d916f909c9a8caff39
Shigeru Kobayashi <shigeru.kb@gmail.com>
2009-04-19 15:58:43 +0900

    second commit.

---
+++ init.lsp
@@ -0,0 +1,270 @@
+;;; -*- encoding: utf-8 -*-
+;;;
+;;; init.lsp --- newLISP initialization file
+;;;
+
+(let ((e (env "NEWLISPDIR")))
+  (unless (directory? e)
+    (println (format "warning: directory %s not found." e))))
+
+(define (find-symbol str (cxt (context)))
+  (sym str cxt nil))
+
+;; (protected? 'define) => true
+;; (primitive? define) => true
+(define (builtin? symbol)
+  (or (primitive? (eval symbol))
+      (starts-with (string symbol) "$") ; $0 ... $15
+      ;; or more symbols
+      (member symbol '(nil true ostype MAIN Tree Class @))
+      ))
+
+(define (user-symbols)
+  (filter (lambda (s) (not (builtin? s)))
+          (symbols)))
+
+(define (apropos str (do-print? nil))
+  "指定した正規表現STRに一致するシンボルを返します. "
+  (let (acc)
+    (dolist (symbol (symbols))
+      (if (find str (string symbol) 1)
+          (push symbol acc -1)))
+    (if (and acc do-print?)
+        (silent (dolist (i acc)
+                  (println i)))
+        acc)))
+
+(define (utf8?)
+  (primitive? MAIN:utf8))
+
+(define (newlisp-version)
+  (let ((version (map int (explode (string (sys-info 7))))))
+    (format "newLISP v%d.%d.%d on %s"
+            (version 0)
+            (version 1)
+            (+ (version 2) (version 3))
+            ostype)))
+
+;; newlisp.h 参照
+(define (type-of x)
+  (let (types '("bool" "bool" "integer" "float" "string"
+                "symbol" "context" "primitive" "cdecl" "stdcall"
+                "quote" "list" "lambda" "macro" "array"))
+    (types (& 0xf ((dump x) 1)))))
+
+(define (load-guiserver)
+  (silent
+    (print "loading guiser...")
+    (load (merge-pathnames "guiserver.lsp" (env "NEWLISPDIR")))
+    (gs:init)
+    (print "done.")))
+
+(define (load-init.lsp)
+  (load (real-path "init.lsp" (env "NEWLISPDIR"))))
+
+(define (xml-parse-file file parse-dtd parse-ns)
+  (xml-parse (read-file file)))
+
+; (global 'progn 't 'null)
+; (constant (global 'cdr) rest)   ; 全ての名前空間で使えるように
+(define-macro (define-cl-function)
+  (constant (global (args 0)) (eval (args 1))))
+(define (null x) (not (true? x)))
+(define t true)                 ; or (constant 't true)
+(define car first)
+(define cdr rest)
+(define defconstant
+  (lambda-macro ()
+    (constant (args 0) (eval (args 1)))))
+(define export global)
+(define progn begin)
+(define (funcall f) (apply f (args)))
+(define let* letn)
+(define lexical-let letex)              ; Emacs cl-package
+(define intern sym)                     ; or make-symbol
+(define symbol-name name)
+(define char-code char)                 ; (char "A") => 65
+(define code-char char)                 ; (char 65)  => "A"
+(define rplaca                          ; (rplaca x y)
+  (lambda-macro ()
+    (setf (first (eval (args 0))) (eval (args 1)))
+    (eval (args 0))))
+;; (subseq nil 0) => nil [CL]
+(define rotatef swap)
+(define complement
+  (lambda-macro ()
+    (letex ((f (args 0)))
+      (lambda ()
+        (not (apply f (args)))))))
+(define-macro (identity) (eval (args 0)))
+
+(define read-from-string read-expr)
+
+;;; @@filesystem, pathname
+(define (merge-pathnames pathname (defaults "."))
+  (real-path (cond ((file? pathname) pathname)
+                   ((regex "^[\\|/]" pathname) pathname)
+                   (true (append defaults "/" pathname)))))
+(define (user-homedir-pathname) (real-path))
+(define (pwd) (real-path))
+(define (namestring pathname) (real-path pathname))
+(define set-default-directory change-dir)
+(define cd change-dir)
+(define cat read-file)
+(define file-exist-p file?)
+(define (probe-file pathname)
+  (or (file? pathname)
+      (real-path pathname)))
+
+(define getenv env)
+
+;;; @@number
+(constant 'most-positive-fixnum 0x7fffffffffffffff) ; 63bit?
+(constant 'most-negative-fixnum 0x8000000000000000)
+(defconstant pi (mul (atan 1) 4))       ; 3.141592654
+(define equal =)
+(define incf inc)
+(define decf dec)
+(define (plusp number) (< 0 number)) ; or (> number) , (sgn number nil nil true)
+(define (minusp number) (< number 0)) ; or (< number) , (sgn number true nil nil)
+(define (ash i cnt) (sgn cnt (>> i (abs cnt)) i (<< i cnt)))
+(define logand &)
+(define logxor ^)
+(define logior |)
+(define lognot ~)
+(define expt pow)
+(define (/= number)
+  "全ての数が異なればtrue."
+  (for-all (lambda (x) (not (= x number))) (args)))
+;; 引数2つしか見てないんじゃないの？
+;; (!= 2 3 4 2)                    ; true
+
+;;; @@list
+(define intersection intersect)
+(define set-difference difference)
+(define butlast chop)
+(define (nthcdr n lst) (slice lst n))
+(define (common-lisp:last lst (n 1))
+  ((- n) lst))
+(define every for-all)
+(define (some f lst)
+  (dolist (obj lst (f obj))))
+(define position find)
+(define find-if exists)
+(define remove-duplicates unique)
+(define (remove item seq)
+  (if (string? seq)
+      (replace item seq "")
+      (replace item seq)))
+(define (remove-if f seq)
+  (filter (lambda (x) (not (f x))) seq))
+(define remove-if-not filter)
+(define common-lisp:delete              ; 破壊的な意味で
+  (lambda-macro ()
+    (replace (eval (args 0)) (eval (args 1)))))
+(define (mapcar f lst)          ; (mapcar function list &rest more-lists)
+  (letn ((lists (cons lst (args)))
+         (minleght (apply min (map length lists))))
+    (apply map (cons f (map (lambda (x)
+                              (slice x 0 minleght))
+                            lists)))))
+;; (mapcar list '(1 2 3 4) '(10 nil 30) '(100 200 300 400 500 600))
+;;=> ((1 10 100) (2 nil 200) (3 30 300))
+
+;;; @@sequence, regexp
+(define split-string parse)
+(define concat string)
+(define copy-seq copy)
+(define string-upcase upper-case)
+(define string-downcase lower-case)
+(define string-capitalize title-case)
+(define compile-regexp regex-comp)
+
+(define (subseq seq start end)
+  (cond (end (slice seq start (- end start)))
+        (true (slice seq start))))
+
+(define (string-match regexp str (start 0) end)
+  (regex regexp (subseq str start end)))
+
+(define (substitute-string str pattern replacement)
+  (replace pattern str replacement))
+
+(define (regex-quote regexp (extended nil)) ; regexp-quote
+  (let (acc)
+    (dolist (x (explode regexp))
+      (if (member x '("$" "^" "." "*" "[" "]" "\\" "+" "?"))
+          (push "\\" acc -1))
+      (push x acc -1))
+    (apply string acc)))
+
+;; 大文字小文字の区別をしない文字列比較
+(define (string-equal string1 string2)
+  (if (regex (string "^" (regex-quote string1) "$") string2 1) true nil))
+
+(define (string-left-trim char-bag str)
+  (if (string? char-bag)
+      (setq char-bag (map char (explode char-bag))))
+  (catch
+      (dostring (c str)
+        (unless (member c char-bag)
+          (throw (slice str $idx))))))
+
+(define (string-right-trim char-bag str)
+  (if (string? char-bag)
+      (setq char-bag (map char (explode char-bag))))
+  (catch
+      (dostring (c (reverse (copy str)))
+        (unless (member c char-bag)
+          (throw (slice str 0 (- (length str) $idx)))))))
+
+(define (string-trim char-bag str)
+  (string-right-trim char-bag (string-left-trim char-bag str)))
+
+;; (define (string-trim char-bag str) (trim str char-bag char-bag))
+;; (define (string-left-trim char-bag str) (trim str char-bag ""))
+;; (define (string-right-trim char-bag str) (trim str "" char-bag))
+
+(define (trim-space str) (trim str))
+(define (trim-whitespace str)
+  (string-trim " \t\r\n" str))
+
+(define (elt seq idx)
+  (char (seq idx)))
+
+;;; @@error
+(define error throw-error)
+
+(context 'ignore-errors)
+(define-macro (ignore-errors:ignore-errors)
+  (letex ((body (cons 'begin (args))))
+    (let (result)
+      (if (catch body 'result) result nil))))
+(context MAIN)
+
+;; 再度エラーを投げるとユーザ定義のエラーになってしまうな
+;; (error-number) の値も変わってしまう
+(context 'unwind-protect)
+(define-macro (unwind-protect:unwind-protect)
+  (letex ((body (args 0))
+          (cleanup-form* (cons 'begin (rest (args)))))
+    (local (*result*)           ; letと何が違う？
+      (if (catch body '*result*)
+          (begin cleanup-form* *result*)
+          (begin cleanup-form* (throw-error *result*))))))
+(context MAIN)
+
+(when (= ostype "Win32")
+  (import "user32" "MessageBoxA")
+  (define (message-box text (title "newLISP"))
+    (let ((MB_OK 0))
+      (MessageBoxA 0 text title MB_OK 1)))
+  )
+
+(define (one-line str) (replace "[\r|\n]" str " " 0))
+
+
+;; (prompt-event (fn (ctx) (string ctx ":" (real-path) "> ")))
+
+(context MAIN)
+;;; init.lsp ends here
---
+++ newlisp.el
@@ -0,0 +1,231 @@
+;;; newlisp.el --- newLISP mode for Emacs
+
+;; This file is NOT part of GNU Emacs.
+
+;; Time-stamp: <2009-04-19T15:14:44>
+
+;; Usage:
+;; (require 'newlisp)
+;; (add-to-list 'auto-mode-alist '("\\.lsp$" . newlisp-mode))
+
+;; Todo:
+;; - newlisp-eval-buffer を独立プロセスで
+;; - newlisp-mode
+;; - 色付け (newlisp-mode.el, elisp-font-lock.el)
+;; - シンボル補完 -> etags, complete-symbol, dabbrev-expand
+;; - pop-to-buffer は縦分割を好む人もいるかも
+;; - elisp の書式チェック (checkdoc) -> 関数にはドキュメントを書けってさ
+;; - defcustomなど
+;; - 構文ステーブルを弄る (`|' はシンボル, `#' はコメント)
+;; - 全ては気の赴くままに
+
+;;; Code:
+(eval-when-compile (require 'cl))
+(require 'comint)
+;; (require 'inf-lisp)
+
+(defvar *newlisp-command* "newlisp")
+;; (defvar *newlisp-command-option* "")
+
+(defvar *newlisp-process-coding-system* '(utf-8 . utf-8))
+
+(defun newlisp-process ()
+  (let ((default-process-coding-system *newlisp-process-coding-system*))
+    (get-buffer-process
+     (make-comint "newlisp" *newlisp-command* nil
+                  ;; newlisp側では`~/'をホームディレクトリとして認識しないので
+                  ;; emacs側で展開しておく
+                  "-C" "-w" (expand-file-name default-directory)))))
+
+(defun newlisp-show-repl (&optional no-focus)
+  (interactive "P")
+  (let ((obuf (current-buffer)))
+    (pop-to-buffer (process-buffer (newlisp-process)))
+    (if no-focus (pop-to-buffer obuf))))
+
+(defalias 'run-newlisp 'newlisp-show-repl)
+
+(defun newlisp-eval (str-sexp)
+  (interactive "snewLISP eval: ")
+  (let ((proc (newlisp-process)))
+    (labels ((sendln (str)
+               (comint-send-string proc (concat str "\n"))))
+      (sendln "[cmd]")
+      (sendln str-sexp)
+      (sendln "[/cmd]"))
+    (newlisp-show-repl t)))
+
+(defun newlisp-eval-region (from to)
+  (interactive "r")
+  (newlisp-eval (buffer-substring-no-properties from to)))
+
+(defun newlisp-eval-last-sexp ()
+  (interactive)
+  (let ((opoint (point)))
+    (unwind-protect
+         (with-syntax-table lisp-mode-syntax-table
+           (newlisp-eval-region (progn
+                                  ;; 'hoge
+                                  (unless (looking-at "\\_<")
+                                    (backward-sexp))
+                                  (point))
+                                (progn
+                                  (forward-sexp)
+                                  (point))))
+      (goto-char (max (point) opoint)))))
+
+(defun newlisp-eval-defun ()
+  (interactive)
+  (save-excursion
+    (mark-defun)
+    (newlisp-eval-region (region-beginning) (region-end))))
+
+(defun newlisp-kill-process ()
+  (interactive)
+  (newlisp-eval "(exit)"))              ; or (kill-process (newlisp-process))
+
+;; eval sync
+(defun newlisp-eval-buffer (arg)
+  (interactive "P")
+  (setq arg (if arg (read-string "newLISP args: ") ""))
+  (shell-command (format "%s \"%s\" %s"
+                         *newlisp-command*
+                         (buffer-file-name)
+                         arg)
+                 "*newLISP output*"))
+
+;; eval async
+(defun newlisp-execute-file (&optional cmd-args)
+  (interactive (list (if current-prefix-arg
+                         (read-string "execute args: " )
+                         "")))
+  ;; (setq arg (if arg (read-string "args: ") ""))
+  (lexical-let ((outbuf (get-buffer-create "*newLISP output*")))
+    (with-current-buffer outbuf (erase-buffer))
+    (set-process-sentinel (start-process-shell-command "newlisp" outbuf
+                                                       *newlisp-command*
+                                                       (buffer-file-name)
+                                                       cmd-args)
+                          #'(lambda (process event)
+                              (cond ((zerop (buffer-size outbuf))
+                                     (kill-buffer outbuf)
+                                     (message "(no output)"))
+                                    (t
+                                     (with-current-buffer outbuf
+                                       (goto-char (point-min))
+                                       (if (< (line-number-at-pos (point-max)) 5)
+                                           (message "%s" (replace-regexp-in-string
+                                                          "\n+$" "" (buffer-string)))
+                                           (pop-to-buffer (process-buffer process))))))))
+    ))
+
+(defun newlisp-complete-symbol (&optional predicate)
+  (interactive)
+  (error "Undefined"))
+
+(defun newlisp-mode-setup ()
+  (setq *newlisp-process-coding-system*
+        (let ((res (shell-command-to-string
+                    (format "%s -n -e \"(primitive? MAIN:utf8)\"" *newlisp-command*))))
+          (if (string-match "true" res)
+              '(utf-8 . utf-8)
+              '(shift_jis . shift_jis)))) ; or 'sjis ?
+  ;; $ newlisp -n -e "(builtin-symbols)"
+  )
+
+(defmacro defindent (operator indentation)
+  `(put ',operator 'lisp-indent-function ',indentation))
+
+(defindent define 1)
+(defindent fn 1)
+(defindent begin 0)
+(defindent local 1)
+(defindent letex 1)
+(defindent for 1)
+(defindent lambda-macro 1)
+(defindent define-macro 1)
+(defindent until 1)
+(defindent letn 1)
+(defindent dostring 1)
+
+(defvar newlisp-mode-hook nil)
+(defvar newlisp-mode-map
+  (let ((map (make-sparse-keymap "newlisp")))
+    (set-keymap-parent map lisp-mode-shared-map)
+    map))
+(define-key newlisp-mode-map "\e\C-x" 'newlisp-eval-defun)
+(define-key newlisp-mode-map "\C-x\C-e" 'newlisp-eval-last-sexp)
+(define-key newlisp-mode-map "\C-c\C-r" 'newlisp-eval-region)
+(define-key newlisp-mode-map "\C-c\C-z" 'newlisp-show-repl)
+(define-key newlisp-mode-map "\e\t" 'newlisp-complete-symbol) ; ESC TAB
+(define-key newlisp-mode-map [f5] 'newlisp-execute-file)
+(define-key newlisp-mode-map "\C-m" 'newline-and-indent)
+
+;;;###autoload
+(defun newlisp-mode ()
+  "Major mode for editing newLISP code to run in Emacs."
+  (interactive)
+  (kill-all-local-variables)
+  (use-local-map newlisp-mode-map)
+  (setq major-mode 'newlisp-mode
+        mode-name "newLISP")
+  (lisp-mode-variables 'and-use-lisp-syntax)
+  ;; `#'もコメント扱いにしたい
+  ;; (set (make-local-variable 'font-lock-keywords-case-fold-search) nil)
+  (run-mode-hooks 'newlisp-mode-hook))
+
+;; $ html2txt $NEWLISPDIR/newlisp_manual.html -o newlisp_manual.txt
+(defvar *newlisp-manual-text* "C:/home/lxuser/newlisp/newlisp_manual.txt")
+
+(defvar *newlisp-symbols*
+  (eval-when-compile
+    (car (read-from-string
+          (shell-command-to-string
+           (format "%s -n -e \"(map name (symbols MAIN))\"" *newlisp-command*))))))
+
+(defun newlisp-manual-from-text (str)
+  (interactive
+   (list (completing-read "newLISP manual: "
+                          *newlisp-symbols*
+                          nil t
+                          (car (member (current-word) *newlisp-symbols*)))))
+  (let ((obuf (current-buffer)))
+    (pop-to-buffer (find-file-noselect *newlisp-manual-text*))
+    (toggle-read-only t)
+    (let ((opoint (point)))
+      (goto-char (point-min))
+      (unless (search-forward (concat "*syntax: (" str) nil 'noerror)
+        (goto-char opoint)
+        (pop-to-buffer obuf)
+        (message "Function Not Found: %s" str)))))
+
+(put 'font-lock-add-keywords 'lisp-indent-function 1)
+;; see lisp-mode.el:91
+(font-lock-add-keywords 'newlisp-mode
+  (list
+   ;; (list "\\<\\(FIXME\\):" 1 font-lock-warning-face 'prepend)
+   (cons (eval-when-compile
+           (regexp-opt *newlisp-symbols* 'words))
+         font-lock-keyword-face)
+   (cons (eval-when-compile
+           (regexp-opt '("define" "lambda" "fn" "define-macro" "lambda-macro") 'words))
+         font-lock-function-name-face)
+   (cons (eval-when-compile
+           (regexp-opt '("nil" "true" "ostype"
+                         "$args" "$idx" "$it" "$main-args" "$prompt-event"
+                         "$" "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9"
+                         "$10" "$11" "$12" "$13" "$14" "$15") 'words))
+         font-lock-constant-face)
+   (cons (eval-when-compile
+           (regexp-opt '("Class" "MAIN" "Tree") 'words))
+         font-lock-builtin-face)
+   (cons (eval-when-compile
+           (regexp-opt '("peek" "fork" "wait-pid" "net-ping" "parse-date") 'words))
+         font-lock-warning-face)
+   ))
+
+;; (defun newlisp-make-regexp-opt (&rest strings) (eval-when-compile (regexp-opt strings 'words)))
+
+(provide 'newlisp)
+
+;;; newlisp.el ends here
